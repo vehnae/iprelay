061172b22c6439d1214d8790226c69996950fdef
Jari Saukkonen <jari.saukkonen@gmail.com>
2021-03-17 06:25:30 +0800

    Get rid of onoff module

--- relays.js
+++ relays.js
@@ -1,29 +1,27 @@
-const Gpio = require('onoff').Gpio;
+const { exec } = require('child_process');
 const config = require('./config');
 
-const gpios = [];
 const state = [];
 
 config.relays.forEach(relay => {
-    gpios.push(new Gpio(relay.pin, 'out'));
     state.push(false);
 })
 
 module.exports = {
   state: () => state,
   on: (relayIndex) => {
-    gpios[relayIndex].writeSync(1);
+    exec(`gpio write ${config.relays[relayIndex].pins} 0`);
     state[relayIndex] = true;
   },
   off: (relayIndex) => {
-    gpios[relayIndex].writeSync(0);
+    exec(`gpio write ${config.relays[relayIndex].pins} 1`);
     state[relayIndex] = false;
   },
   pulse: (relayIndex) => {
-    gpios[relayIndex].writeSync(1);
+    exec(`gpio write ${config.relays[relayIndex].pins} 0`);
     state[relayIndex] = true;
     setTimeout(() => {
-      gpios[relayIndex].writeSync(0);
+      exec(`gpio write ${config.relays[relayIndex].pins} 1`);
       state[relayIndex] = false;
     }, 1000);
   }
